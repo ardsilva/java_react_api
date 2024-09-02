d93fb2d011d9fd3f03dfac2f8c433c092e8dcc50
Alexandre Silva <ardsilva87@gmail.com>
2024-09-02 10:46:34 +0100

    feat: project initial commit

---
+++ backend/src/main/java/com/okta/developer/jogtours/JogtoursApplication.java
@@ -0,0 +1,13 @@
+package com.okta.developer.jogtours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogtoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogtoursApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/otka/developer/jogtours/JogtoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otka.developer.jogtours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogtoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
